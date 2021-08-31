ad3ec0faa0a687e7df725bbba3a3960f8b950fc0
Zach Schiff <zaschiff@gmail.com>
2021-08-31 08:04:14 -0600

    Completed Chapter 9 and section 10.1

--- bundle.js
+++ bundle.js
@@ -1,14 +1,28 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+// node syntaxt to obtain another JavaScript file
 let Phrase = require("zaschiff-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+// function to test for a plaindrome through the webpage
+function palindromeTester(event) {
+    event.preventDefault();
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
-} else {
-    alert(`"${phrase.content}" is not a palindrome.`);
-}
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}" is a palindrome!</strong>`;
+    } else {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}" is not a palindrome.</strong>`;
+    }
+};
+
+// allows file to wait until the DOm is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // obtaining the button itself from the DOM and checking for an event to fire
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    });
+});
 },{"zaschiff-palindrome":2}],2:[function(require,module,exports){
 // makes the phrase object usable in other packages
 module.exports = Phrase;
@@ -47,7 +61,11 @@ function Phrase(content) {
 
     // Returns true for a pailindrome, false otherwise.
     this.palindrome = function palindrome() {
-        return this.processedContent() === this.processedContent().reverse();
+        if (this.processedContent()) {
+            return this.processedContent() === this.processedContent().reverse();
+        } else {
+            return false;
+        }
     }
 
     // Exercise 7.1.1.1
--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+// node syntaxt to obtain another JavaScript file
 let Phrase = require("zaschiff-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+// function to test for a plaindrome through the webpage
+function palindromeTester(event) {
+    event.preventDefault();
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
-} else {
-    alert(`"${phrase.content}" is not a palindrome.`);
-}
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}" is a palindrome!</strong>`;
+    } else {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}" is not a palindrome.</strong>`;
+    }
+};
+
+// allows file to wait until the DOm is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // obtaining the button itself from the DOM and checking for an event to fire
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    });
+});
